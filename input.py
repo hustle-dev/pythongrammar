5e4c43bc047efcce0b2ef8ad169ced0fe9674f57
Quant-Kor <53992007+Quant-Kor@users.noreply.github.com>
2020-12-31 18:39:07 +0900

    input func

---
+++ input.py
@@ -0,0 +1,13 @@
+a = input()
+print(a)
+
+number = input("숫자를 입력하세요: ")
+print(number)
+
+# print함수의 사용
+'''
+큰따옴표(")로 둘러싸인 문자열은 + 연산과 동일하다 --> print("life" "is" "too short")
+문자열 띄어쓰기는 콤마로 한다 --> print("life", "is", "too short")
+한 줄에 결괏값 출력하기 --> print(i, end= '')
+'''
+
